1270296d6057d06ba8e53cb8482ce4bd1451493c
Barbod Garsha <barbodgarshasbi79@gmail.com>
2023-03-08 18:14:41 +0100

    the component is finished

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 radio_btn = document.querySelector('[data-radio-btn]')
 radio_btn__options = radio_btn.querySelectorAll('[data-radio-btn-option]')
-radio_btn__selected_val = ""
+radio_btn__selected_val = ''
+
+submit_btn = document.querySelector('[data-submit-btn]')
+
+card_rate = document.querySelector('[data-card-rate]')
+card_thank_you = document.querySelector('[data-card-thank-you]')
+card__selected_rating = document.querySelector('[data-card-selected-rating]')
+
+
 
 radio_btn.addEventListener('click', function(e) {
     if(e.target.hasAttribute('data-radio-btn-option')) {
@@ -10,4 +18,13 @@ radio_btn.addEventListener('click', function(e) {
         e.target.classList.toggle('my-radio-btn__option--selected')
         radio_btn__selected_val = e.target.querySelector('[data-radio-btn-option-val]').innerHTML
     } 
+})
+
+submit_btn.addEventListener('click', function(e) {
+
+    if(radio_btn__selected_val != '') {
+        card_rate.classList.toggle('card--hide')
+        card_thank_you.classList.toggle('card--hide')
+        card__selected_rating.innerHTML = radio_btn__selected_val
+    }
 })
